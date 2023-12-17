10657b1b1b542a9a7c0bbc100232412b08d6916c
abdusamatovs <147515892+abdusamatovs@users.noreply.github.com>
2023-12-17 14:36:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// let age = "Hi";
+
+// console.log(age);
+
+// age = "Hello";
+
+// console.log(age);
+
+// console.log(101 == 2);
+
+// let random = Math.random() * 5;
+
+// console.log(random);
+
+// let random = 3.9
+
+// let circle = Math.round(random);
+
+// console.log(circle);
+
+// let down = Math.floor(random);
+
+// console.log(down);
+
+// let max = Math.ceil(random);
+
+// console.log(max);
+
+let box = prompt("Ismingizni kiriting ");
+let box2 = prompt("Familyangizni kiriting");
+let box3 = prompt("Yoshingizni kiriting ");
+let box4 = prompt("Qaysi kursda o'qimoqchisiz?");
+
+confirm("Ma'lumotlaringiz hammasi to'g'rimi");
+
+let random = Math.random();
+
+alert(`1. Ismingiz:  ${box} \n2. Familyangiz:  ${box2} \n3. Yoshingiz:  ${box3} \n4. Kurs:  ${box4}\n\n5. Sizning ID raqamingiz:  ${random}   
+          `);
